b6bd96a5407998bb5d96061a694814fbb44a1c9d
James Tan <jamestyj@gmail.com>
2013-10-10 17:34:05 +0100

    Install MongoHacker

--- my_cookbooks/utils/recipes/default.rb
+++ my_cookbooks/utils/recipes/default.rb
@@ -8,3 +8,14 @@ package 'tmux'
 
 # Install MongoDB plugin for dstat
 execute "wget -P /usr/share/dstat/ https://raw.github.com/gianpaj/dstat/master/plugins/dstat_mongodb_cmds.py"
+
+# Install MongoHacker
+execute [
+  'wget -P /tmp https://github.com/TylerBrock/mongo-hacker/archive/master.zip',
+  'unzip /tmp/master.zip -d /tmp/',
+  'cd /tmp/mongo-hacker-master',
+  'make',
+  'ln mongo_hacker.js /home/ec2-user/.mongorc.js',
+  'chown ec2-user:    /home/ec2-user/.mongorc.js',
+  'rm -rf /tmp/{mongo-hacker-master,master.zip}'
+].join(' && ')
