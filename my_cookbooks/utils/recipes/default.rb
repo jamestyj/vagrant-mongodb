823140e4c1b3852168d910dcebe904e6651c7cb4
James Tan <james.tan@mongodb.com>
2015-04-27 23:53:43 +0200

    More updates

--- my_cookbooks/utils/recipes/default.rb
+++ my_cookbooks/utils/recipes/default.rb
@@ -4,34 +4,25 @@ package 'htop'
 package 'sysstat'
 
 # Install other useful utils
+package 'git'
 package 'tmux'
 package 'tree'
 
-execute 'install_dstat_with_mongodb_plugin' do
-  command 'wget -P /usr/share/dstat/ https://raw.github.com/gianpaj/dstat/master/plugins/dstat_mongodb_cmds.py'
-  not_if { FileTest.directory?('/usr/share/dstat/') }
-end
-
 execute 'install_mongo_hacker' do
   command [
-    'wget -P /tmp https://github.com/TylerBrock/mongo-hacker/archive/master.zip',
-    'unzip /tmp/master.zip -d /tmp/',
-    'cd /tmp/mongo-hacker-master',
-    'make',
-    'ln mongo_hacker.js /home/ec2-user/.mongorc.js',
-    'chown ec2-user:    /home/ec2-user/.mongorc.js',
-    'rm -rf /tmp/{mongo-hacker-master,master.zip}'
+    'git clone https://github.com/TylerBrock/mongo-hacker.git',
+    'cd mongo-hacker',
+    'make install'
   ].join(' && ')
-  not_if { ::File.exists?('/home/ec2-user/.mongorc.js') }
 end
 
 execute 'clean_up_vagrant_omnibus' do
-  command 'rm -f /home/ec2-user/install.sh'
+  command 'rm -f /home/centos/install.sh'
 end
 
 cookbook_file 'public_ip' do
-  path  '/home/ec2-user/public_ip'
-  owner 'ec2-user'
-  group 'ec2-user'
+  path  '/home/centos/public_ip'
+  owner 'centos'
+  group 'centos'
   mode  '0755'
 end
